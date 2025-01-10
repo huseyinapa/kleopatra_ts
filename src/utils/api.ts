434bedcecd8104b706e57da4848b31bfcf47a18c
Hüseyin APA <81553048+huseyinapa@users.noreply.github.com>
2025-01-10 19:01:59 +0300

    NodeEnv olarak değiştirildi

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,13 @@
-export const NODE_ENV = process.env.NEXT_PUBLIC_NODE_ENV as
+export const NodeEnv = process.env.NEXT_PUBLIC_NODE_ENV as
   | "development"
   | "production";
 
 export const api_url = {
   development: "http://3.124.99.216",
   production: "https://api.gonenkleopatra.com",
-}[NODE_ENV];
+}[NodeEnv];
 
 export const pay_url = {
   development: "https://pay.huseyinapa.com",
   production: "https://pay.gonenkleopatra.com",
-}[NODE_ENV];
+}[NodeEnv];
